4fdb0feeff3a1604e9336bf88ff46c11a665642e
Sam Vredenburgh <sam.vburgh@gmail.com>
2017-05-04 10:55:01 -0500

    basic page setup with background img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import Page from './components/Page';
 
 class App extends Component {
   render() {
     return (
-      <Header text="Whether Weather" />
+      <div>
+        <Header text="Whether Weather" /> 
+        <Page />
+      </div>
     )
   }
 }
---
+++ src/components/Page.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Page extends Component {
+	render () {
+		return (
+			<div className="page-container">
+				<div>
+				</div>
+			</div>
+		)
+	}
+}
+
+module.exports = Page;
