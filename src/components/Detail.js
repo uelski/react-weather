c8bab8d5d0e3a9f6523536184f09e50ebc80d58d
Sam Vredenburgh <sam.vburgh@gmail.com>
2017-05-10 09:48:54 -0500

    link to city in detail component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class Detail extends Component {
+	constructor (props) {
+		super(props);
+
+		this.routeBack = this.routeBack.bind(this);
+	}
+	routeBack(city) {
+		this.props.history.push({
+	      pathname: '/forecast',
+	      search: '?place=' + city
+	    })
+	}
 	render() {
 		var props = this.props.location.state
 		return (
 			<div className="detail-container">
-				<h1>{props.city}</h1>
+				<h1 onClick={this.routeBack.bind(this, props.city)}>{props.city}</h1>
 				<h3 className="detail-date">{props.dt_formatted}</h3>
 				<div>{props.temp.day}</div>
 				<div>{props.weather[0].main}</div>
