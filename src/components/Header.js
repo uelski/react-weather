f7444359b18a43ecc64ab7e3329ef583b3063a7c
Sam Vredenburgh <sam.vburgh@gmail.com>
2017-05-10 09:38:03 -0500

    header link working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 	render () {
 		return (
 			<div className="header-container">
-				<h1>{this.props.text}</h1>
+				<Link className="header-link"
+					to={{pathname: '/'}}>
+					<h1>{this.props.text}</h1>
+				</Link>	
 				{this.props.children}
 			</div>
 		)
